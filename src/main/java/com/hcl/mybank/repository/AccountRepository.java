a5fb3f1e7a2b14c14145d01e60b298c1dcfa7db6
rksiet2 <rksiet2@gmail.com>
2019-08-03 13:31:43 +0530

    Account Summary comit

--- src/main/java/com/hcl/mybank/repository/AccountRepository.java
+++ src/main/java/com/hcl/mybank/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.hcl.mybank.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.hcl.mybank.dto.AccountSummaryDto;
@@ -11,6 +12,7 @@ import com.hcl.mybank.entity.Account;
 @Repository
 public interface AccountRepository  extends JpaRepository<Account, Long>{
 
+	@Query("")
 	public List<AccountSummaryDto> findUserSummary();
 
 }
