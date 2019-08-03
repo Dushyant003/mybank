f7b82389f4b510f5d21250205c9eba0ee654c7ca
ajayarankeri <ajayarankeri@gmail.com>
2019-08-03 17:11:30 +0530

    conflicts resovled

--- src/main/java/com/hcl/mybank/service/TransactionService.java
+++ src/main/java/com/hcl/mybank/service/TransactionService.java
@@ -6,11 +6,11 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-<<<<<<< HEAD
+
 import com.hcl.mybank.dto.ResponseDto;
-=======
+
 import com.hcl.mybank.dto.AccountsDetailsDto;
->>>>>>> 612cccf3db457e060fc29c32b65d11452f76516d
+
 import com.hcl.mybank.dto.TransactionDto;
 import com.hcl.mybank.exception.ResourceNotFoundException;
 import com.hcl.mybank.exception.TransactionLimitOverException;
@@ -19,14 +19,13 @@ import com.hcl.mybank.exception.TransactionLimitOverException;
 @Service
 public interface TransactionService {
 
-<<<<<<< HEAD
+
 	public ResponseDto fundTransfer(TransactionDto transactionDto) throws ResourceNotFoundException;
-=======
-	public Transaction fundTransfer(TransactionDto transactionDto);
+	
 	public boolean validtransaction1(long accountNo) throws ResourceNotFoundException, TransactionLimitOverException;
 	
 	public List<AccountsDetailsDto> getTransactionDetails(long id);
->>>>>>> 612cccf3db457e060fc29c32b65d11452f76516d
+
 
 	public boolean validtransaction(long accountNo) throws ResourceNotFoundException,TransactionLimitOverException;
 
