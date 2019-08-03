be04eb9620f5daec3aabe42c2e83d11b8e3018e5
ajayarankeri <ajayarankeri@gmail.com>
2019-08-03 13:43:58 +0530

    account repo committed

--- src/main/java/com/hcl/mybank/repository/AccountRepository.java
+++ src/main/java/com/hcl/mybank/repository/AccountRepository.java
@@ -1,20 +1,16 @@
 package com.hcl.mybank.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.hcl.mybank.dto.AccountSummaryDto;
 import com.hcl.mybank.entity.Account;
 
 @Repository
 public interface AccountRepository  extends JpaRepository<Account, Long>{
 
-
-	@Query("")
-	public List<AccountSummaryDto> findUserSummary();
+	/*
+	 * @Query("") public List<AccountSummaryDto> findUserSummary();
+	 */
 
 
 }
