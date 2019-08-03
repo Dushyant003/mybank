7235d04920d7a35ab2206f4713bedba39a7b0265
RajithaHCL <rajitha.battini@hcl.com>
2019-08-03 14:13:51 +0530

    add new chnages

--- src/main/java/com/hcl/mybank/controller/AccountController.java
+++ src/main/java/com/hcl/mybank/controller/AccountController.java
@@ -1,27 +1,14 @@
 package com.hcl.mybank.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.hcl.mybank.dto.TransactionDto;
-import com.hcl.mybank.entity.Transaction;
-import com.hcl.mybank.service.TransactionService;
 
 @RestController
 @RequestMapping("")
 public class AccountController {
 	
-	@Autowired
-	TransactionService transactionService;
 	
-	@PostMapping("/fundtransfer")
-	public void fundTransfer(@RequestBody TransactionDto transactionDto) {
-	  Transaction transactionDetails=	transactionService.fundTransfer(transactionDto);
-		
-		
-	}
+	
+	
 
 }
---
+++ src/main/java/com/hcl/mybank/repository/AccountRepository.java
@@ -0,0 +1,17 @@
+package com.hcl.mybank.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hcl.mybank.entity.Account;
+import com.hcl.mybank.entity.Customer;
+
+@Repository
+	public interface AccountRepository  extends JpaRepository<Account, Long>{
+
+		Account findByCustomerId(Customer customer);
+
+	}
+
+
+
