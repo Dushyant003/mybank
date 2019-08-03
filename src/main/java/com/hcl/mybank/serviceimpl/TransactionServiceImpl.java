48a1b23a1d12b622f4d8b21f13767156bad5a94e
ajayarankeri <ajayarankeri@gmail.com>
2019-08-03 13:09:02 +0530

    transaction service created

--- src/main/java/com/hcl/mybank/serviceimpl/TransactionServiceImpl.java
+++ src/main/java/com/hcl/mybank/serviceimpl/TransactionServiceImpl.java
@@ -1,14 +1,16 @@
 package com.hcl.mybank.serviceimpl;
 
 import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import com.hcl.mybank.dto.TransactionDto;
 import com.hcl.mybank.entity.Account;
 import com.hcl.mybank.entity.Transaction;
 import com.hcl.mybank.repository.AccountRepository;
+import com.hcl.mybank.repository.TransactionRepository;
 import com.hcl.mybank.service.TransactionService;
 
 @Component
@@ -16,15 +18,25 @@ public class TransactionServiceImpl implements TransactionService{
 	
 	@Autowired
 	AccountRepository accountRepository;
+	
+	@Autowired
+	TransactionRepository transactionRepository;
 
 	
 	@Override
 	public Transaction fundTransfer(TransactionDto transactionDto) {
 		Optional<Account> fromAccountDetails=accountRepository.findById(transactionDto.getFromAccountId());
 		Optional<Account> toAccountDetails=accountRepository.findById(transactionDto.getToAccountId());
-		
+		Transaction transaction;
 		
 		Boolean validate=	validateFundTransfer(transactionDto);
+		
+		if(validate) {
+			transaction=new Transaction();
+			BeanUtils.copyProperties(transactionDto, transaction);
+			
+			
+		}
 		
 		
 		return null;
