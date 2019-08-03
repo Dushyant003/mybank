d0edd9b54759bc286f8c11a0c763d2c8239c37aa
sandhyapisal <sandhyadnyanadeo.r@hcl.com>
2019-08-03 12:29:34 +0530

    add new chnages

---
+++ src/main/java/com/hcl/mybank/service/TransactionService.java
@@ -0,0 +1,6 @@
+package com.hcl.mybank.service;
+
+public interface TransactionService {
+
+	public boolean validtransaction(long accountNo);
+}
---
+++ src/main/java/com/hcl/mybank/serviceimpl/TransactionServiceImpl.java
@@ -0,0 +1,17 @@
+package com.hcl.mybank.serviceimpl;
+
+import org.hibernate.Transaction;
+import org.omg.IOP.TransactionService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionServiceImpl implements TransactionService {
+
+ public boolean validtransaction(long accountNo)
+   {
+		 
+    }
+
+
+
+}
