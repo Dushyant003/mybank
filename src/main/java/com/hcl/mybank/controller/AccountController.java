2bb80f3c1990ba529eba3ded6e083888b069b7fd
rksiet2 <rksiet2@gmail.com>
2019-08-03 13:03:03 +0530

    Account Summary comit

--- src/main/java/com/hcl/mybank/controller/AccountController.java
+++ src/main/java/com/hcl/mybank/controller/AccountController.java
@@ -1,10 +1,10 @@
 package com.hcl.mybank.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/hcl/mybank/repository/AccountRepository.java
+++ src/main/java/com/hcl/mybank/repository/AccountRepository.java
@@ -1,12 +1,16 @@
 package com.hcl.mybank.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hcl.mybank.dto.AccountSummaryDto;
 import com.hcl.mybank.entity.Account;
 
 @Repository
 public interface AccountRepository  extends JpaRepository<Account, Long>{
 
+	public List<AccountSummaryDto> findAnalysisReportWeekly();
 
 }
